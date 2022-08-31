a323f41e3bebd8b87eb1ce952206b71da9c0a1e8
nehause <wlsgnlwo@gmail.com>
2022-08-31 15:59:42 +0900

    dao

--- src/main/java/com/kbbook/shop/modules/codeGroup/CodeGroupDao.java
+++ src/main/java/com/kbbook/shop/modules/codeGroup/CodeGroupDao.java
@@ -1,5 +1,21 @@
 package com.kbbook.shop.modules.codeGroup;
 
-public class CodeGroupDao {
+import java.util.List;
 
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CodeGroupDao {
+	
+	@Inject
+	@Resource(name = "sqlSession")
+	private SqlSession sqlSession;
+	
+	private static String namespace = "com.kbbook.shop.modules.codegroup.CodeGroupMapper";
+	
+	public List<CodeGroup> selectList(){ return sqlSession.selectList(namespace + ".selectList", ""); }
 }
