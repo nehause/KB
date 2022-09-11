ad42618d251809c7de38bab43d983865ffbc9be6
nehause <wlsgnlwo@gmail.com>
2022-09-11 16:29:59 +0900

    maincontorller

--- src/main/java/com/kbbook/shop/HomeController.java
+++ src/main/java/com/kbbook/shop/HomeController.java
@@ -1,88 +1,83 @@
-package com.kbbook.shop;
-
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-@Controller
-public class HomeController {
-			
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
+/*
+ * package com.kbbook.shop;
+ * 
+ * import java.text.DateFormat; import java.util.Date; import java.util.Locale;
+ * 
+ * import org.slf4j.Logger; import org.slf4j.LoggerFactory; import
+ * org.springframework.stereotype.Controller; import
+ * org.springframework.ui.Model; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RequestMethod;
+ * 
+ * @Controller public class HomeController {
+ * 
+ * private static final Logger logger =
+ * LoggerFactory.getLogger(HomeController.class);
+ * 
+ *//**
 	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	@RequestMapping(value = "/a", method = RequestMethod.GET)
-	public String a(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		return "a";
-	}
-	@RequestMapping(value = "/b", method = RequestMethod.GET)
-	public String b(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		return "b";
-	}
-	@RequestMapping(value = "/c", method = RequestMethod.GET)
-	public String c(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		return "c";
-	}
-	@RequestMapping(value = "/d", method = RequestMethod.GET)
-	public String d(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		return "d";
-	}
-	@RequestMapping(value = "/e", method = RequestMethod.GET)
-	public String e(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		return "e";
-	}
-	@RequestMapping(value = "/codeGroupList", method = RequestMethod.GET)
-	public String codeGroupList(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		return "codeGroupList";
-	}
-	@RequestMapping(value = "/codeGroupRegForm", method = RequestMethod.GET)
-	public String codeGroupRegForm(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		return "codeGroupRegForm";
-	}
-	
-	@RequestMapping(value="/")
-	public String main() throws Exception {
-		
-		return "infra/member/user/main";
-	}
-	@RequestMapping(value="/loginForm")
-	public String LoginForm() throws Exception {
-		
-		return "infra/member/user/loginForm";
-	}
-}
+	 *//*
+		 * @RequestMapping(value = "/home", method = RequestMethod.GET) public String
+		 * home(Locale locale, Model model) {
+		 * logger.info("Welcome home! The client locale is {}.", locale);
+		 * 
+		 * Date date = new Date(); DateFormat dateFormat =
+		 * DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		 * 
+		 * String formattedDate = dateFormat.format(date);
+		 * 
+		 * model.addAttribute("serverTime", formattedDate );
+		 * 
+		 * return "home"; }
+		 * 
+		 * @RequestMapping(value = "/a", method = RequestMethod.GET) public String
+		 * a(Locale locale, Model model) {
+		 * logger.info("Welcome home! The client locale is {}.", locale);
+		 * 
+		 * return "a"; }
+		 * 
+		 * @RequestMapping(value = "/b", method = RequestMethod.GET) public String
+		 * b(Locale locale, Model model) {
+		 * logger.info("Welcome home! The client locale is {}.", locale);
+		 * 
+		 * return "b"; }
+		 * 
+		 * @RequestMapping(value = "/c", method = RequestMethod.GET) public String
+		 * c(Locale locale, Model model) {
+		 * logger.info("Welcome home! The client locale is {}.", locale);
+		 * 
+		 * return "c"; }
+		 * 
+		 * @RequestMapping(value = "/d", method = RequestMethod.GET) public String
+		 * d(Locale locale, Model model) {
+		 * logger.info("Welcome home! The client locale is {}.", locale);
+		 * 
+		 * return "d"; }
+		 * 
+		 * @RequestMapping(value = "/e", method = RequestMethod.GET) public String
+		 * e(Locale locale, Model model) {
+		 * logger.info("Welcome home! The client locale is {}.", locale);
+		 * 
+		 * return "e"; }
+		 * 
+		 * @RequestMapping(value = "/codeGroupList", method = RequestMethod.GET) public
+		 * String codeGroupList(Locale locale, Model model) {
+		 * logger.info("Welcome home! The client locale is {}.", locale);
+		 * 
+		 * return "codeGroupList"; }
+		 * 
+		 * @RequestMapping(value = "/codeGroupRegForm", method = RequestMethod.GET)
+		 * public String codeGroupRegForm(Locale locale, Model model) {
+		 * logger.info("Welcome home! The client locale is {}.", locale);
+		 * 
+		 * return "codeGroupRegForm"; }
+		 * 
+		 * @RequestMapping(value="/") public String main() throws Exception {
+		 * 
+		 * return "infra/member/user/main"; }
+		 * 
+		 * @RequestMapping(value="/loginForm") public String LoginForm() throws
+		 * Exception {
+		 * 
+		 * return "infra/member/user/loginForm"; } }
+		 */
